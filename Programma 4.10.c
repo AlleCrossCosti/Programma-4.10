34cc4fd9aa2f1cf2238f76f2b7d8c1f852cd25d7
Alle Cross Costi <costialle2@hotmail.it>
2018-02-03 16:23:01 +0100

    Initial commit

---
+++ Programma 4.10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main () {
+  
+  int intNUM;
+  
+  print (" Digitare un numero decimale:  ");
+  scanf ("%d " , &intNUM);
+  printf ("Numero in esadecimale: %#x\n", intNUM);
+  
+  return 0;
+  
+  }
